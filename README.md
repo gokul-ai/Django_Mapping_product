ae6017d531ee9ddd49004a57fe62de375b52be6b
gokul <gk3903@gmail.com>
2020-06-28 20:53:10 +0530

    updated register form

--- README.md
+++
@@ -1,1 +0,0 @@
-# Django_Mapping_product
--- mysite/core/views.py
+++ mysite/core/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
 
+from mysite.core.forms import SignUpForm
 # Create your views here.
 
 def home(request):
     return render(request,"home.html")
 
 
+
 def signup(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("home")
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
     else:
-        form = UserCreationForm()
-    return render(request,"registration/signup.html",{
-        "form":form
-    })
+        form = SignUpForm()
+    return render(request, "registration/signup.html", {'form': form})
     
